945ccfe6f8fdc1e086da2af35994586c397a156c
ndikandik020@gmail.com <ndikandik020@gmail.com>
2019-01-06 09:53:27 +0700

    asoy

--- projectAndika/src/projectandika/ProjectAndika.java
+++ projectAndika/src/projectandika/ProjectAndika.java
@@ -15,7 +15,7 @@ public class ProjectAndika {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here by andikaa ganteng
+        // TODO code application logic here by andikaa ganteng pisan
     }
     
 }
